3ecebfb1236d0a69386878f3c473970b31e11cf7
Cameron Hall <gravatacs@gmail.com>
2020-02-11 09:16:37 -0600

    removed JavaFx folder

--- JavaFx Example/src/NewWindow.java
+++
@@ -1,8 +0,0 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-
-public class NewWindow extends Application
-{
-	@Override
-	public void start(final Stage primarystage)
-}
